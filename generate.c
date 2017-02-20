1ac8f71f095310b46cd60d1e58c813e818305b68
Brian Scott Goffman <brian.goffman+1@gmail.com>
2017-02-20 07:09:00 +0000

    Add additional files

---
+++ generate.c
@@ -0,0 +1,52 @@
+/**
+ * generate.c
+ *
+ * Generates pseudorandom numbers in [0,MAX), one per line.
+ *
+ * Usage: generate n [s]
+ *
+ * where n is number of pseudorandom numbers to print
+ * and s is an optional seed
+ */
+ 
+#define _XOPEN_SOURCE
+
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// upper limit on range of integers that can be generated
+#define LIMIT 65536
+
+int main(int argc, string argv[])
+{
+    // Check if command line has 2 or 3 arguments
+    if (argc != 2 && argc != 3)
+    {
+        printf("Usage: ./generate n [s]\n");
+        return 1;
+    }
+
+    // Convert value to int, store in n
+    int n = atoi(argv[1]);
+
+    // Generate random number using seed else seed with current time
+    if (argc == 3)
+    {
+        srand48((long) atoi(argv[2]));
+    }
+    else
+    {
+        srand48((long) time(NULL));
+    }
+
+    // Print n random numbers multiplied by fixed variable LIMIT
+    for (int i = 0; i < n; i++)
+    {
+        printf("%i\n", (int) (drand48() * LIMIT));
+    }
+
+    // success
+    return 0;
+}
